e6f5df58f04de457e34f6005ce7b7e6f51e14c2d
Leo Dearden <leo@leodearden.com>
2014-03-02 17:54:46 +0000

    Generalise to build anything ending -stl.scad.

--- build.py
+++ build.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
-import sys, os
-print sys.path
+import sys, os, glob
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/fabricate')
-print sys.path
 from fabricate import *
 
-sources = ['gear-mother-mold-base', 'gear-mother-mold-lid', 'gear-mother-mold-wall', 'gear-mother-mold-sub-base-back', 'gear-mother-mold-sub-base-left', 'gear-mother-mold-sub-base-right', 'gear-mother-mold-sub-runners']
+sources = glob.glob('*-stl.scad')
 
 def build():
     for source in sources:
-        run('openscad', source + '.scad', '-o', source + '.stl')
+        run('openscad', source, '-o', source[:-9] + '.stl')
 
 def clean():
     autoclean()
